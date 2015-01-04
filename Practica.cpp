8a82c26f040cb407977b8eeb520abb2beb09ba0d
Xavier Conejo <xavier.maria.conejo@est.fib.upc.edu>
2015-01-05 00:26:01 +0100

    Esto es un mensaje

--- Practica.cpp
+++ Practica.cpp
@@ -11,15 +11,6 @@ struct Vuelo
 	int td;
 };
 
-vector<Vuelo> leer()
-{
-	vector<Vuelo> v;
-	Vuelo v;
-	while (cin >> v.o >> v.d >> v.to >> v.td)
-		v.push_back(v);
-	return v
-}
-
 void concatena(queue<int>& a, queue<int>& b)
 {
 	while (!b.empty())
@@ -29,37 +20,56 @@ void concatena(queue<int>& a, queue<int>& b)
 	}
 }
 
-queue<int> compatible1(int i, const vector<Vuelo>& v)
+vector<Vuelo> leer()
 {
-
+	vector<Vuelo> v;
+	Vuelo v;
+	while (cin >> v.o >> v.d >> v.to >> v.td)
+		v.push_back(v);
+	return v
 }
 
 queue<int> compatible1(int i, const vector<Vuelo>& v)
 {
+	queue<int> q;
+	for (int j = 0; j < v.size(); ++j)
+	{
+		if ((i != j) and (v[i].d == v[j].o) and ((v[i].td + 15) >= v[j].to))
+			q.push(2*j);
+	}
+	return q;
+}
+
+queue<int> compatible2(int i, const vector<Vuelo>& v)
+{
+	queue<int> q;
 	
+	return q;
 }
 
 vector< vector<int> > grafo(const vector<Vuelo>& v, bool x)
 {
 	int n = 2*v.size();
 	vector< vector< pair<int, int> > > mat(n + 2, vector< pair<int, int> > (n + 2, pair<int, int>(0, 0)));
+	//mat es el grafo que representa el conjunto de vuelos: first -> lower bound; second -> capacity
 	for (int i = 0; i < v.size(); ++i)
 	{
-		mat[i][i + 1] = pair<int, int>(1, 1);
-		mat[n][i] = pair<int, int>(0, 1);
-		mat[i + 1][n + 1] = pair<int, int>(0, 1);
+		mat[2*i][2*i + 1] = pair<int, int>(1, 1);
+		mat[n][2*i] = pair<int, int>(0, 1);
+		mat[2*i + 1][n + 1] = pair<int, int>(0, 1);
 		queue<int> q = compatible1(i, v);
-		if (x)
-			concatena(q, compatible2(i, v));
+		if (x) concatena(q, compatible2(i, v));
 		while (!q.empty())
 		{
-			mat[i + 1][q.front()] = pair<int, int>(0, 1);
+			mat[2*i + 1][q.front()] = pair<int, int>(0, 1);
 			q.pop();
 		}
 	}
 
 	n = mat.size();
 	vector< vector<int> > m(n + 2, vector<int> (n + 2, 0));
+	//m es el grafo que ya se han quitado los lower bound y a la 
+	//vez se ha hecho la transformacion a circulacion de demandas
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < n; ++j)
