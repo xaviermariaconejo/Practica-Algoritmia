3f11a3168fd364f3f724100393abae1bfa259021
TekuDruida <TekuDruida@bitbucket.org>
2015-01-05 19:27:59 +0100

    maxFlowCalculed

--- Practica.cpp
+++ Practica.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <stack>
 #include <queue>
+#include <list>
+#include <limits>
 using namespace std;
 
 struct Vuelo
@@ -14,11 +16,11 @@ struct Vuelo
 
 vector<Vuelo> leer()
 {
-	vector<Vuelo> v;
+	vector<Vuelo> vuelos;
 	Vuelo v;
 	while (cin >> v.o >> v.d >> v.to >> v.td)
-		v.push_back(v);
-	return v
+		vuelos.push_back(v);
+	return vuelos;
 }
 
 stack<int> compatible1(int i, const vector<Vuelo>& v)
@@ -27,12 +29,12 @@ stack<int> compatible1(int i, const vector<Vuelo>& v)
 	for (int j = 0; j < v.size(); ++j)
 	{
 		if ((i != j) and (v[i].d == v[j].o) and ((v[i].td + 15) >= v[j].to))
-			q.push(2*j);
+			s.push(2*j);
 	}
 	return s;
 }
 
-queue<int> compatible2(int i, const vector<Vuelo>& v)
+stack<int> compatible2(int i, const vector<Vuelo>& v)
 {
 	stack<int> r, s;
 	s.push(i);
@@ -111,9 +113,48 @@ void BFS(const vector< vector<int> >& mat, int i)
 	}
 }
 
-int EdmonsKarp(vector< vector<int> >& mat)
+int EdmonsKarp(vector< vector<int> >& mat, int s, int t)
 {
+	vector< vector<int> > res = vector< vector<int> >(mat.size()); //graf residual
+	vector<int> p = vector<int>(mat.size(), -1); //taula dels "pares", contindra el cami.
+	vector<int> f = vector<int>(mat.size()); //capacitat del cami. Necesaria per calcular el flux que queda en l'aresta i quant torna.
 
+    p[s] = s; //indicamos que es el inicio.
+    f[s] = numeric_limits<int>::max();
+
+    queue<int> q;
+    q.push(s);
+
+    while (not q.empty()) {
+    	int u = q.front();
+    	for(int v : mat[u]) {
+    		if (mat[u][v] > 0 and p[v] == -1) { //Capacitat-Fluxe es > 0 i no s'ha visitat encara.
+    			p[v] = u;
+    			if (f[u] <= mat[u][v]) f[v] = f[u];
+    			else f[v] = mat[u][v]; //agafem el minim.
+
+    			if(v != t) q.push(v); //sino hem arribat al final seguim buscant el cami.
+    			else { //recorrem el cami al reves i anem calculant el nou graf residu:
+    				while(p[v] != v) { //l'unic que el seu pare es ell mateix es s.
+    					u = p[v];
+    					res[u][v] += f[t];
+    					res[v][u] -= f[t];
+    					v = u;
+    				}
+    			}
+
+    		}
+    	}
+    }
+    if(p[t] == -1) {//no existeix cap cami desde s fins a t:
+    	int suma = 0;
+    	for (int flux : res[s]) //calculem el fluxe que surt de s, que sera el que arribara a t, per tant el fluxe maxim.
+    	{
+    		suma = suma + flux;
+    	}
+    	return suma;
+    }
+	
 }
 
 int otroAlgoritmo(vector< vector<int> >& mat)
@@ -128,7 +169,7 @@ vector< list<int> > resolver(const vector< vector<int> >& mat, bool x)
 	for (k = 1; k <= (mat.size() - 4)/2 and !b; ++k)
 	{
 		int cont;
-		mat[n][n - 1] = m[n - 1][n + 1] = k;
+		mat[n][n - 1] = mat[n - 1][n + 1] = k;
 		vector< vector<int> > aux = mat;
 		if (x) cont = EdmonsKarp(aux);
 		else cont = otroAlgoritmo(aux);
@@ -137,6 +178,17 @@ vector< list<int> > resolver(const vector< vector<int> >& mat, bool x)
 	vector< list<int> > v(k);
 	//sacar la lista de viajes q hace cada piloto
 	return v;
+}
+
+void escriure(vector< list<int> > sol) {
+	cout << sol.size() << endl; //esto es el numero de pilotos que seria la k que resuelve el problema.
+
+	for(int i = 0; i < sol.size(); ++i) {
+		for(list<int>::iterator j = sol[i].begin(); j != sol[i].end(); ++j) {
+			cout << *j << ""; //TODO: No se si funciona
+		}
+		cout << endl;
+	}
 }
 
 int main()
