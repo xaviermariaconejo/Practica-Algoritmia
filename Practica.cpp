292b39bbf0a4ee7cc335a30fc8fde35f4663981a
Xavier Conejo <xavier.maria.conejo@est.fib.upc.edu>
2015-01-06 19:03:42 +0100

    ya ta

--- Practica.cpp
+++ Practica.cpp
@@ -92,7 +92,7 @@ vector< vector<int> > grafo(const vector<Vuelo>& v, bool x)
 	return m;
 }
 
-vector< list<int> > calculaViajes(vector< vector<int> > &res, int a, int b, int k)
+vector< list<int> > calcularViajes(vector< vector<int> > &res, int a, int b, int k)
 {
 	vector< list<int> > vl(k);
 	int j = 0;
@@ -136,7 +136,8 @@ vector< list<int> > calculaViajes(vector< vector<int> > &res, int a, int b, int 
 
 void BFS(const vector< vector<int> >& mat, int s, int t, vector<int>& p, vector< vector<int> >& res)
 {
-	vector<int> f(mat.size()); //capacitat del cami. Necesaria per calcular el flux que queda en l'aresta i quant torna.
+	vector<int> f(mat.size());
+	//capacitat del cami. Necesaria per calcular el flux que queda en l'aresta i quant torna.
 
     p = vector<int>(mat.size(), -1);
     p[s] = s; //indicamos que es el inicio.
@@ -145,29 +146,32 @@ void BFS(const vector< vector<int> >& mat, int s, int t, vector<int>& p, vector<
     queue<int> q;
     q.push(s);
 
-    while (not q.empty()) {
-        
+    while (not q.empty()) {        
     	int u = q.front();
         q.pop();
 
     	for(int v = 0; v < mat[u].size(); ++v) {
 
-    		if (mat[u][v]-res[u][v] > 0 and p[v] == -1) { //Capacitat-Fluxe es > 0 i no s'ha visitat encara.
+    		if (mat[u][v]-res[u][v] > 0 and p[v] == -1) {
+    		//Capacitat-Fluxe es > 0 i no s'ha visitat encara.
     			p[v] = u;
     			if (f[u] <= mat[u][v]-res[u][v]) f[v] = f[u];
     			else f[v] = mat[u][v]-res[u][v];
     			//agafem el minim.
 
-    			if(v != t) q.push(v); //sino hem arribat al final seguim buscant el cami.
+    			if(v != t) q.push(v); 
+    			//sino hem arribat al final seguim buscant el cami.
 
-    			else { //recorrem el cami al reves i anem calculant el nou graf residu:
+    			else {
+    			//recorrem el cami al reves i anem calculant el nou graf residu:
     				while(p[v] != v) { //l'unic que el seu pare es ell mateix es s.
     					u = p[v];
     					res[u][v] += f[t];
     					res[v][u] -= f[t];
     					v = u;
     				}
-    				return; //TODO: si ya hemos encontrado un camino que llega a t salimos de la funcion.
+    				return;
+    				//TODO: si ya hemos encontrado un camino que llega a t salimos de la funcion.
     			}
     		}
     	}
@@ -181,27 +185,43 @@ int EdmonsKarp(vector< vector<int> >& mat, int s, int t, vector< vector<int> >& 
 
     do {
     	BFS(mat,s,t,p,res);
-    } while (p[t] != -1);
-    
+    } while (p[t] != -1);    
     //no existeix cap cami desde s fins a t, no es pot augmentar mes:
+
 	int suma = 0;
-	for (int j = 0; j < res[s].size(); ++j) //calculem el fluxe que surt de s, que sera el que arribara a t, per tant el fluxe maxim.
+	for (int j = 0; j < res[s].size(); ++j)
+	//calculem el fluxe que surt de s, que sera el que arribara a t, per tant el fluxe maxim.
 		suma = suma + res[s][j];
 
 	return suma;	
 }
 
-/*int cercaDicotomica() {
-
-}*/
-
 vector< list<int> > resolver(vector< vector<int> >& mat)
 {
-	bool b = false;
+	int s = mat.size() - 2;
+	int t = mat.size() - 1;
+	vector< vector<int> > res;
+
+	int e = 1;
+	int d = (mat.size() - 4)/2;
+	int k = (mat.size() - 4)/2;
+	mat[mat.size() - 2][mat.size() - 4] = mat[mat.size() - 3][mat.size() - 1] = k;
+	int max = EdmonsKarp(mat,s,t,res);
+	while (e <= d)
+	{
+		k = (i + d)/2;
+		int aux = EdmonsKarp(mat,s,t,res);
+		if (aux == max)
+            d = k - 1;
+        else
+        	e = k + 1;
+	}
+	/*bool b = false;
 	int k, s, t;
 	s = mat.size() -2;
 	t = mat.size() -1;
-	vector< vector<int> > res; //esto contendra el grafo residual para calcular que vuelo hace cada piloto una vez hallada la k optima.
+	vector< vector<int> > res;
+	//esto contendra el grafo residual para calcular que vuelo hace cada piloto una vez hallada la k optima.
 
 	for (k = 1; k <= (mat.size() - 4)/2 and !b; ++k)
 	{
@@ -209,8 +229,8 @@ vector< list<int> > resolver(vector< vector<int> >& mat)
 		mat[mat.size() - 2][mat.size() - 4] = mat[mat.size() - 3][mat.size() - 1] = k;
 		b = ((mat.size() - 4)/2 + k) == EdmonsKarp(mat,s,t,res);
 	}
-
-	return calculaViajes(res, mat.size()-4, mat.size()-3, k - 1);
+	*/
+	return calcularViajes(res, mat.size()-4, mat.size()-3, k);
 	//sacar la lista de viajes q hace cada piloto
 }
 
